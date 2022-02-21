44710ec3c78a231608d93a4bdf471f31a3d8e55d
Karina Kumykova <k.kumykova@gmail.com>
2022-02-21 11:59:19 +0000

    add create_spend_chart function; the result looks similar, the test is still complaining about spacing

--- budget.py
+++ budget.py
@@ -1,5 +1,6 @@
 import collections
 
+
 class Category:
     def __init__(self, name):
         self.name = name
@@ -46,8 +47,7 @@ class Category:
 
 
 def create_spend_chart(categories_list):
-    bar_chart = "Percentage spent by category"
-    percentage_line = ''
+    bar_chart_description = "Percentage spent by category"
 
     total_spend = 0
     separate_amounts = []
@@ -64,12 +64,56 @@ def create_spend_chart(categories_list):
     spend_by_cat = dict(counter)
 
     for k, v in spend_by_cat.items():
-        spend_by_cat[k] = round(v/abs(total_spend) * 100) - round(v/abs(total_spend) * 100) % 10
+        spend_by_cat[k] = int(str(round(v / abs(total_spend) * 100) - round(v / abs(total_spend) * 100) % 10)[0])
 
-    return spend_by_cat
+    key_list = []
+    lens = []
+    percentage_lengths = []  # [8, 1]
+    for k, v in spend_by_cat.items():
+        key_list.append(k)
+        lens.append(len(k))
+        percentage_lengths.append(v)
 
+    amend_keys = [(k + " " * (max(lens) - len(k))) if len(k) < max(lens) else k for k in key_list]
 
+    amend_keys_list = list(amend_keys[0])
+    first_key = ["     " + str(amend_keys_list[i]) for i in range(len(amend_keys_list))]
+    amend_keys[0] = first_key
 
+    column_descriptions = ""
+    for i in range(max(lens)):  # columns
+        for key in amend_keys:  # rows
+            column_descriptions += key[i] + "  "
+        column_descriptions += "\n"
+
+    raw_dots = [l * "o" for l in percentage_lengths]
+    amend_raw_dots = [(" " * (11 - len(dots)) + dots) if len(dots) < 11 else dots for dots in raw_dots]
+
+    percentage_bar = []
+    for p in range(100, -10, -10):
+        percentage_bar.append((str(p) + "| ").rjust(5))
+    # print("percentage_bar:", percentage_bar)
+
+    # Constructing the first line of the percentage bar
+    # Get the first string from the amend_raw_dots
+    first_spend_line = amend_raw_dots[0]
+    # print("first_spend_line:", len(first_spend_line))
+    # split first_spend_line into a list of characters including the empty spaces
+    first_spend_line_list = list(first_spend_line)
+    # combine two lists - first_spend_line_list and percentage_bar - into pairs
+    first_bar_line = [str(percentage_bar[i]) + first_spend_line_list[i] for i in range(len(percentage_bar))]
+    # Replace the first item in the amend_raw_dots with the constructed first_bar_line
+    amend_raw_dots[0] = first_bar_line
+    # print("amend_raw_dots:", amend_raw_dots)
+
+    percentage_dots = ""
+    for i in range(11):  # columns
+        for dot in amend_raw_dots:  # rows
+            percentage_dots += dot[i] + "  "
+        percentage_dots += "\n"
+    percentage_dots += "    " + "-" * (len(raw_dots) * 2 + 3) + "\n"
+
+    return bar_chart_description + "\n" + percentage_dots + column_descriptions
 
 
 if __name__ == "__main__":
@@ -81,16 +125,4 @@ if __name__ == "__main__":
     food.deposit(900, "deposit")
     food.withdraw(5, "milk, cereal, eggs, bacon, bread")
     food.transfer(20, entertainment)
-    # print(food.get_balance())
-    print(food.ledger)
-    print(entertainment.ledger)
-    #print(food)
-
     print(create_spend_chart([food, entertainment]))
-
-    # print(entertainment.get_balance())
-
-    # print(entertainment.check_funds(30))
-    # print(food.check_funds(20))
-    # print(entertainment.balance)
-    # print(entertainment.ledger)
